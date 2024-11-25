3e0b98a7d086566da7b27372837eeebbc7169c38
Lucas Alisson <you@example.com>
2024-11-25 10:43:31 -0300

    Subindo Avaliativa AP2 completa para o repositório

---
+++ src/MeuComponente.js
@@ -0,0 +1,19 @@
+
+// Novo Componente 
+import React from 'react';
+
+function MeuComponente() {
+  return (
+    <div>
+      <h2>Componente Secundário</h2>
+      <p>componente criado dentro do React.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MeuComponente;
